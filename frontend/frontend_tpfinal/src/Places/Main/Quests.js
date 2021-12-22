5b561b07bdc78e23e57dcf44776b30ace72e3a53
Chris Nelson K. KOM <chrisnelsonkouokam@gmail.com>
2021-12-21 21:32:33 -0500

    tpFinal.0

--- frontend/frontend_tpfinal/src/Places/Main/Quests.js
+++ frontend/frontend_tpfinal/src/Places/Main/Quests.js
@@ -1,61 +1,65 @@
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
-import axios from "axios"
+import axios from "axios";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useState } from "react";
 import { Box } from "@mui/system";
-import  Button  from "@mui/material/Button";
+import Button from "@mui/material/Button";
 
 function Quests(props) {
-  const {token} = props ;
+  const { token } = props;
   const [quest, setQuest] = useState([]);
-  let {questId} = useParams();
-  const fo = async () => {
+  let { questId } = useParams();
+  const f = async () => {
     try {
       const questRequest = await axios({
         method: "get",
         url: "http://localhost:3001/quests",
         headers: {
-          Authorization: "BEARER " + token ,
-        }
+          Authorization: "BEARER " + token,
+        },
       });
-       setQuest(questRequest.data);
+      setQuest(questRequest.data);
     } catch (e) {
       console.log(e);
     }
   };
-  fo();
- 
-   const HandleSubmit  = (id) => {
+  f();
+
+  const HandleSubmit = (id) => {
     questId = id;
     //const data = new FormData(event.currentTarget);
     // eslint-disable-next-line no-console
-    useEffect(()=> {
-        const completeRequest = axios({
-          method: "post",
-          url: `http://localhost:3001/complete/${questId}`,
-          headers: {
-            Authorization: "BEARER " + token ,
-          },
-        });
-         console.log(completeRequest.status);
+    useEffect(() => {
+      const completeRequest = axios({
+        method: "post",
+        url: `http://localhost:3001/complete/:${questId}`,
+        headers: {
+          Authorization: "BEARER " + token,
+        },
+      });
+      console.log(completeRequest.status);
     }, []);
-   
   };
 
-
-  const myQuest = quest.map((a) =><Paper
-  elevation={10}
-  sx={{
-    padding: "15px",
-    heigth: "400px",
-  }}
->   <Typography variant="h6">{a.name}</Typography>
-    <Typography >level {a.level}</Typography>
-    <Typography>Complete for {a.completionXp} xp</Typography>
-    <Button onSubmit={() => HandleSubmit(a._id)} noValidate >COMPLETE</Button>
-  </Paper>);
+  const myQuest = quest.map((a) => (
+    <Paper
+      elevation={10}
+      sx={{
+        padding: "15px",
+        heigth: "400px",
+      }}
+    >
+      {" "}
+      <Typography variant="h6">{a.name}</Typography>
+      <Typography>level {a.level}</Typography>
+      <Typography>Complete for {a.completionXp} xp</Typography>
+      <Button onSubmit={() => HandleSubmit(a._id)} noValidate>
+        COMPLETE
+      </Button>
+    </Paper>
+  ));
   return (
     <Paper
       elevation={10}
